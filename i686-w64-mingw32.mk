c6010d6988cb460dc18591801267ebef89a06e73
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-02-28 13:59:00 +0200

    mingw32: Get rid of extra library paths

--- i686-w64-mingw32.mk
+++ i686-w64-mingw32.mk
@@ -10,14 +10,9 @@ SRC= \
 ./src/lib_json/json_reader.cpp \
 ./src/lib_json/json_writer.cpp
 
-LIBS= \
--L/usr/i686-w64-mingw32/sys-root/mingw/lib \
--L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
--L/usr/i686-w64-mingw32/lib/
-
 all:
 	mkdir -p $(TARGET_DIR)
-	$(CXX) -shared -o $(TARGET_DIR)/$(TARGET) $(CFLAGS) $(SRC) $(LIBS) -Wl,--out-implib,$(TARGET_DIR)/$(TARGET).a
+	$(CXX) -shared -o $(TARGET_DIR)/$(TARGET) $(CFLAGS) $(SRC) -Wl,--out-implib,$(TARGET_DIR)/$(TARGET).a
 
 .PHONY: clean
 clean:
